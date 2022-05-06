d508b027ed6a494e39851e8356281db54cfe4209
Ulysses Almeida <ulysses.almeida@gmail.com>
2022-05-06 12:14:25 -0400

    AtomicCube class creatoin

---
+++ src/AtomicCube.py
@@ -0,0 +1,29 @@
+
+
+class AtomicCube(object):
+    def __init__(self):
+        self.upper_face = "YL"
+        self.down_face = "WT"
+        self.front_face = "BL"
+        self.back_face = "GR"
+        self.right_face = "RD"
+        self.left_face = "ON"
+
+    def __str__(self):
+        ret = ""
+        ret += "   |%s|\n" % (self.back_face)
+        ret += "   |%s|\n" % (self.upper_face)
+        ret += "|%s|%s|%s|\n" % (self.left_face, self.front_face, self.right_face)
+        ret += "   |%s|" % (self.down_face)
+        return ret
+
+    def up_rotate(self):
+        pass
+
+
+def test():
+    ac = AtomicCube()
+    print(ac)
+
+if __name__ == "__main__":
+    test()
