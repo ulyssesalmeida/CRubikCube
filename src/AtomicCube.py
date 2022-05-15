629fd877660183787cbe438301dcafb7af3eec62
Ulysses Almeida <ulysses.almeida@gmail.com>
2022-05-15 10:34:56 -0400

    Implemented up_rotate and down_rotate methods with some tests

--- src/AtomicCube.py
+++ src/AtomicCube.py
@@ -17,13 +17,44 @@ class AtomicCube(object):
         ret += "   |%s|" % (self.down_face)
         return ret
 
-    def up_rotate(self):
-        pass
+    def up_rotate(self, clockwise=True):
+        if clockwise:
+            tmp = self.front_face
+            self.front_face = self.right_face
+            self.right_face = self.back_face
+            self.back_face = self.left_face
+            self.left_face = tmp
+        else:
+            tmp = self.front_face
+            self.front_face = self.left_face
+            self.left_face = self.back_face
+            self.back_face = self.right_face
+            self.right_face = tmp
+
+    def down_rotate(self, clockwise=True):
+        self.up_rotate(True^clockwise)
 
 
 def test():
     ac = AtomicCube()
     print(ac)
+    print ("--")
+    ac.up_rotate()
+    print(ac)
+    print ("--")
+    ac.up_rotate()
+    print(ac)
+    print ("--")
+    ac.up_rotate(clockwise=False)
+    print(ac)
+
+    print ("--")
+    ac.down_rotate()
+    print (ac)
+    print ("--")
+    ac.down_rotate(clockwise=False)
+    print (ac)
+
 
 if __name__ == "__main__":
     test()
